99c211b8616cbe2e1e2143437a64d6f8890ca247
Mominulislam1234423 <mominulislamshawon88@gmail.com>
2026-02-16 16:02:19 +0600

    add ptoduct title, price, rate, categori

--- JS/script.js
+++ JS/script.js
@@ -36,15 +36,15 @@ const displayAllProduct = (products) => {
         div.innerHTML = `
         <div class="space-y-2 border-2 rounded-md mt-4">
                 <div>
-                    <img class="h-60 w-full bg-gray-100 p-6" src="${product.image}">
+                    <img class="h-80 w-full bg-gray-100 p-6" src="${product.image}">
                 </div>
                 <div class="p-2 space-y-2">
                     <div class="flex justify-between itmes-center">
-                        <button class="bg-blue-100 text-blue-800 text-sm px-2 rounded-xl">men's Clothing</button>
-                        <span><i class="fa-solid fa-star text-yellow-500"></i>3.9(120)</span>
+                        <button class="bg-blue-100 text-blue-800 text-sm px-2 rounded-xl">${product.category}</button>
+                        <span><i class="fa-solid fa-star text-yellow-500"></i>${product.rating.rate}(${product.rating.count})</span>
                     </div>
-                    <p class="text-md font-bold text-gray-500">Fjallraven - foldsack No 1...</p>
-                    <h4 class="text-lg font-bold">$109.95</h4>
+                    <p class="text-md font-bold text-gray-500">${product.title}</p>
+                    <h4 class="text-lg font-bold">$${product.price}</h4>
                     <div class="flex justify-between">
                         <button class="space-x-2 border-2 py-1 px-4 rounded-md"><i
                                 class="fa-solid fa-circle-info"></i>Details</button>
