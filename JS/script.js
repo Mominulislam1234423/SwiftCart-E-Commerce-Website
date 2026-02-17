fc6e80aa986cad3c213ea6d7c39da4dcdaeb9dc2
Mominulislam1234423 <mominulislamshawon88@gmail.com>
2026-02-17 16:08:37 +0600

    Model section complete

--- JS/script.js
+++ JS/script.js
@@ -36,6 +36,38 @@ const loadcategori = (category) => {
 };
 
 
+const loadWord = (id) => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+        .then(res => res.json())
+        .then(data => displayWOrd(data))
+}
+
+const displayWOrd = (word) => {
+    const detalisBox = document.getElementById("details-Continar");
+    detalisBox.innerHTML = `
+     <div class="space-y-2 border-2 rounded-md mt-4">
+                <div>
+                    <img class=" h-auto w-full bg-gray-100 p-6" src="${word.image}">
+                </div>
+                <div class="p-2 space-y-2">
+                    <div class="flex justify-between itmes-center">
+                        <button class="bg-blue-100 text-blue-800 text-sm px-2 rounded-xl">${word.category}</button>
+                        <span><i class="fa-solid fa-star text-yellow-500"></i>${word.rating.rate}(${word.rating.count})</span>
+                    </div>
+                    <p class="text-md font-bold text-gray-500 truncate">${word.title}</p>
+                    <h4 class="text-lg font-bold">$${word.price}</h4>
+                    <div class="flex justify-between">
+                        <button onclick="loadWord(${word.id})" class="space-x-2 btn border-2 py-1 px-4 rounded-md"><i
+                                class="fa-solid fa-circle-info"></i>Details</button>
+                        <button onclick="addItam()" class="space-x-2 border-2 btn py-1 px-4 rounded-md"><i
+                                class="fa-solid fa-cart-shopping"></i>add</button>
+                    </div>
+                </div>
+            </div>
+    `;
+    document.getElementById("my_modal_5").showModal();
+}
+
 
 const displayProduct = (products) => {
     const allProductBtn = document.getElementById("all-product");
@@ -57,7 +89,7 @@ const displayProduct = (products) => {
                     <p class="text-md font-bold text-gray-500 truncate">${product.title}</p>
                     <h4 class="text-lg font-bold">$${product.price}</h4>
                     <div class="flex justify-between">
-                        <button onclick="loadWordDetail(${products.id})" class="space-x-2 btn border-2 py-1 px-4 rounded-md"><i
+                        <button onclick="loadWord(${product.id})" class="space-x-2 btn border-2 py-1 px-4 rounded-md"><i
                                 class="fa-solid fa-circle-info"></i>Details</button>
                         <button onclick="addItam()" class="space-x-2 border-2 btn py-1 px-4 rounded-md"><i
                                 class="fa-solid fa-cart-shopping"></i>add</button>
